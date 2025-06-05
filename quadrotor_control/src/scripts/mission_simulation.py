8117d9c62ce6503ca6be3a9816993601992789ed
shaurya <shauryajha235@yahoo.com>
2025-06-05 15:39:59 -0700

    added pitch and roll stability

--- quadrotor_control/src/scripts/mission_simulation.py
+++ quadrotor_control/src/scripts/mission_simulation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
+from math import sqrt
 import numpy as np
 from enum import Enum
 from std_msgs.msg import Float64MultiArray
@@ -30,6 +31,12 @@ class Controller:
 
         self.prev_y_error = 0.0
         self.y_error_integral = 0.0
+
+        self.prev_roll_error = 0.0
+        self.roll_error_integral = 0.0
+
+        self.prev_pitch_error = 0.0
+        self.pitch_error_integral = 0.0
 
         self.dt = 0.01 # 10ms / iteration
 
@@ -81,6 +88,35 @@ class Controller:
         
         return sway_command
 
+    def roll(self, current_roll_velocity: float) -> float:
+        Kp, Ki, Kd = 1.0, 0.0, 0.0
+
+        roll_error = -current_roll_velocity
+        roll_error_derivative = (roll_error - self.prev_roll_error) / self.dt
+        self.roll_error_integral += roll_error * self.dt
+
+        roll_command = (Kp * roll_error +
+                        Kd * roll_error_derivative +
+                        Ki * self.roll_error_integral)
+        
+        self.prev_roll_error = roll_error
+
+        return roll_command
+    
+    def pitch(self, current_pitch_velocity: float) -> float:
+        Kp, Ki, Kd = 1.0, 0.0, 0.0
+
+        pitch_error = -current_pitch_velocity
+        pitch_error_derivative = (pitch_error - self.prev_pitch_error) / self.dt
+        self.pitch_error_integral += pitch_error * self.dt
+
+        pitch_command = (Kp * pitch_error +
+                         Kd * pitch_error_derivative +
+                         Ki * self.pitch_error_integral)
+        
+        self.prev_pitch_error = pitch_error
+
+        return pitch_command
 
     def normalize_angle(self, angle: float) -> float:
         while angle < -np.pi:
@@ -122,21 +158,22 @@ class Drone:
         self.current_attitude = np.zeros(3)
         self.current_angular_velocity = Vector3()
 
-        # State machine
-        self.mission_state = MissionState.TAKEOFF
-        self.state_start_time = rospy.Time.now()
-
-        self.state_transitions = {
-            MissionState.TAKEOFF: [self.at_operating_altitude, MissionState.CRUISE],
-            MissionState.CRUISE: [self.at_target_xy, MissionState.HOVER],
-            MissionState.HOVER: [self.hover_complete, MissionState.DESCEND],
-            MissionState.DESCEND: [self.at_ground_level, MissionState.LANDED],
-            MissionState.LANDED: (None, None)
-        }
-
         # Controller
         self.controller = Controller(self.target_position)
         self.control_timer = rospy.Timer(rospy.Duration(1/100), self.fly) # 100Hz update rate
+
+        # State machine
+        self.mission_state = MissionState.TAKEOFF
+        self.set_state_target()
+        self.state_start_time = rospy.Time.now()
+
+        self.state_transitions = {
+            MissionState.TAKEOFF: (self.at_operating_altitude, MissionState.CRUISE),
+            MissionState.CRUISE: (self.at_target_xy, MissionState.HOVER),
+            MissionState.HOVER: (self.hover_complete, MissionState.DESCEND),
+            MissionState.DESCEND: (self.at_ground_level, MissionState.LANDED),
+            MissionState.LANDED: (None, None)
+        }
 
     def imu_callback(self, data: Imu):
         self.current_angular_velocity = data.angular_velocity
@@ -159,10 +196,12 @@ class Drone:
         #rospy.loginfo(f"Current heading: {self.current_attitude[2]:.2f}")
 
     def at_operating_altitude(self):
-        return abs(self.target_position.z - self.current_position.z) <= 0.05
+        return abs(self.target_position.z - self.current_position.z) <= 0.1
     
     def at_target_xy(self):
-        return abs(self.target_position.x - self.current_position.x) <= 0.05 and abs(self.target_position.y - self.current_position.y) <= 0.05
+        dx = self.target_position.x - self.current_position.x
+        dy = self.target_position.y - self.current_position.y
+        return sqrt(dx**2 + dy**2) <= 0.05
     
     def hover_complete(self):
         elapsed_time = rospy.Time.now() - self.state_start_time
@@ -199,21 +238,20 @@ class Drone:
             target = self.target_position
 
         elif self.mission_state == MissionState.DESCEND:
-            target = Point(self.current_position.x, self.current_position.y, 0.0)
+            target = Point(self.target_position.x, self.target_position.y, 0.0)
         
         elif self.mission_state == MissionState.LANDED:
             target = self.current_position
 
         self.controller.update_target(target)
-        rospy.logwarn(f"Updated target to: x={target.x:.2f}, y={target.y:.2f}, z={target.z:.2f}")
-
+        #rospy.logwarn(f"Updated target to: x={target.x:.2f}, y={target.y:.2f}, z={target.z:.2f}")
 
     def fly(self, event):
         self.update_mission_state()
 
         if self.mission_state == MissionState.LANDED:
             self.command.force.z = 0.0
-            self.command.forxe.x, self.command.force.y = 0.0, 0.0
+            self.command.force.x, self.command.force.y = 0.0, 0.0
             #self.motor_msg.data = [0.0, 0.0, 0.0, 0.0]
 
         else:
@@ -222,6 +260,10 @@ class Drone:
             self.command.force.y = self.controller.sway(self.current_position.y)
             # thrust_command = self.controller.thrust(self.current_position.z)
             # self.motor_msg.data = [self.base_speed + thrust_command] * 4
+
+        # Stabilize pitch and roll using rate controllers
+        self.command.torque.x = self.controller.pitch(self.current_angular_velocity.x)
+        self.command.torque.y = self.controller.roll(self.current_angular_velocity.y)
 
         #self.motor_pub.publish(self.motor_msg)
         self.cmd_pub.publish(self.command)
