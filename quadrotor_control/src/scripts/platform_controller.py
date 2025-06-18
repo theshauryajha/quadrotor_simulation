a09dfdc970b2d60f8882a0177dc5641bc54cfb8e
shaurya <shauryajha235@yahoo.com>
2025-06-17 17:23:20 -0700

    added moving platform

---
+++ quadrotor_control/src/scripts/platform_controller.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Twist
+
+
+class PlatformController:
+    def __init__(self):
+        rospy.init_node('platform_controller', anonymous=True)
+
+        self.odom_sub = rospy.Subscriber('/platform/odom', Odometry, self.odom_callback)
+        self.cmd_pub = rospy.Publisher('/platform/cmd_vel', Twist, queue_size=10)
+
+        self.current_position = Point()
+        self.command = Twist()
+
+    def odom_callback(self, data):
+        self.current_position = data.pose.pose.position
+
+    def move(self):
+        self.command.linear.x = 2.0
+        self.command.linear.y = 2.0
+
+        self.cmd_pub.publish(self.command)
+
+
+if __name__ == "__main__":
+    try:
+        platform_controller = PlatformController()
+        platform_controller.move()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
