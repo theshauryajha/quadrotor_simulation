674625667a42bd6e65aba4d0f0f49fd9968fbad1
shaurya <shauryajha235@yahoo.com>
2025-06-17 18:21:19 -0700

    changed platform control mechanism

--- quadrotor_control/src/scripts/platform_controller.py
+++ quadrotor_control/src/scripts/platform_controller.py
@@ -15,12 +15,14 @@ class PlatformController:
         self.current_position = Point()
         self.command = Wrench()
 
+        self.control_timer = rospy.Timer(rospy.Duration(1/100), self.move) # 100Hz update rate
+
     def odom_callback(self, data):
         self.current_position = data.pose.pose.position
 
-    def move(self):
-        self.command.force.x = 2.0
-        self.command.force.y = 2.0
+    def move(self, event):
+        self.command.force.x = 500.0
+        self.command.force.y = 500.0
 
         self.cmd_pub.publish(self.command)
 
@@ -28,7 +30,6 @@ class PlatformController:
 if __name__ == "__main__":
     try:
         platform_controller = PlatformController()
-        platform_controller.move()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
